5b93dffad3cd5c7c58c82e1007e474b3d8e6493e
alinef2104 <alinefcordeiro04@gmail.com>
2025-06-21 19:10:42 -0300

    commit atividade

--- resources/views/veiculo-listar.blade.php
+++ resources/views/veiculo-listar.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Lista de Veículos</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">''
     <style>
         body {
             background-color: #f8f9fa;
