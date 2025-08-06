85adafd7948f2209c570a8ff9dbdfd92d51c922a
alinef2104 <alinefcordeiro04@gmail.com>
2025-08-06 16:26:19 -0300

    aula 06-08-25

---
+++ app/Http/Controllers/UsuarioController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Usuario;
+use App\Http\Requests\StoreUsuarioRequest;
+use App\Http\Requests\UpdateUsuarioRequest;
+use Iluminate\Http\Request;
+
+class UsuarioController extends Controller
+{
+    function registrar(Request $dados) {
+        return $dados;
+    }
+
+    function login(Request $dados) { }
+
+    function logout(Request $dados) { }
+    
+    function desativarConta(Request $dados) { }
+    
+    function fotoUpload(Request $dados) { }
+    
+    function editar(Request $dados) { }
+
+    function perfil(Request $dados) { }
+}
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('usuario')->group(function(){
+    Route::post('registrar-se', [App\Http\Controllers\UsuarioController::class, 'registrar']);
+    Route::post('login', [App\Http\Controllers\UsuarioController::class, 'login']);
+    Route::post('logout', [App\Http\Controllers\UsuarioController::class, 'logout']);
+    Route::post('desativar-conta', [App\Http\Controllers\UsuarioController::class, 'desativar']);
+    Route::post('foto-upload', [App\Http\Controllers\UsuarioController::class, 'fotoUpload']);
+    Route::post('editar', [App\Http\Controllers\UsuarioController::class, 'editar']);
+    Route::post('perfil', [App\Http\Controllers\UsuarioController::class, 'perfil']);
+});
