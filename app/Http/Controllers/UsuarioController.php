811510663d80037d32a097672220679373c55319
alinef2104 <alinefcordeiro04@gmail.com>
2025-08-08 16:27:46 -0300

    08-08

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -3,14 +3,42 @@
 namespace App\Http\Controllers;
 
 use App\Models\Usuario;
-use App\Http\Requests\StoreUsuarioRequest;
-use App\Http\Requests\UpdateUsuarioRequest;
-use Iluminate\Http\Request;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class UsuarioController extends Controller
 {
-    function registrar(Request $dados) {
-        return $dados;
+    public function registrar(Request $request)
+    {
+
+        
+        $request->validate([
+            'nome' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:usuarios',
+            'senha' => 'required|string|min:8',
+        ]);
+
+        $usuario = Usuario::create([
+            'nome' => $request->nome,
+            'email' => $request->email,
+            'senha' => Hash::make($request->senha),
+            'foto' => 'https://i.ytimg.com/vi/nkl98s8I8bw/maxresdefault.jpg',
+            'status' => 'ativo',
+            'ativado' => true,
+        ]);
+
+        // Criar token de acesso
+        $token = '123';
+
+        return response()->json([
+            'message' => 'Usuário registrado com sucesso',
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+            'usuario' => $usuario
+        ], 201);
     }
 
     function login(Request $dados) { }
